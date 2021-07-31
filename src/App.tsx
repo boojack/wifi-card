89fdd0ae0fe4fbbd95b3fa9ca5fe2e7777e6a31d
LeeShuang <sli4@ebay.com>
2021-07-31 13:02:51 +0800

    -polish styles;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card, Form, Input, Layout, Space, Image, Button } from 'antd'
+import { Card, Form, Input, Layout, Image, Button, Switch, Divider } from 'antd'
 import { Content } from 'antd/lib/layout/layout'
 import qrcode from 'qrcode'
 import './App.css'
@@ -22,7 +22,8 @@ const initialProps: AppProps = {
 
 function App() {
   const [props, setProps] = useState<AppProps>(initialProps)
-  const [showEditor, setShowEditorStatus] = useState<Boolean>(true)
+  const [showEditor, setShowEditorStatus] = useState<boolean>(true)
+  const [showWifiInfo, setShowWifiInfoStatus] = useState<boolean>(true)
 
   const handleFormChange = async (values: AppProps) => {
     values = {
@@ -31,11 +32,11 @@ function App() {
     }
 
     let url = ''
-    if (values.wifiName.length > 0 && values.wifiPasswd.length >= 8) {
+    if (validWifiInfo()) {
       url = await qrcode.toDataURL(
         `WIFI:T:WPA;S:${values.wifiName};P:${values.wifiPasswd};;`,
         {
-          width: 360,
+          width: window.innerWidth * 0.9,
         }
       )
     }
@@ -44,7 +45,7 @@ function App() {
   }
 
   const handlePrintBtnClick = () => {
-    if (props.wifiName.length === 0 || props.wifiPasswd.length < 8) {
+    if (!validWifiInfo()) {
       window.alert('请输入正确的 WiFi 名称和密码！')
       return
     }
@@ -56,55 +57,83 @@ function App() {
     }, 0)
   }
 
+  const validWifiInfo = (): boolean => {
+    if (props.wifiName.length === 0 || props.wifiPasswd.length < 8) {
+      return false
+    }
+    return true
+  }
+
   return (
     <>
-      <Layout className="app-container" hasSider={false}>
-        <h1>WiFi Card</h1>
-        <Content>
-          <Space
+      <Layout className="app-wrapper" hasSider={false}>
+        <Content className="app-container">
+          <Form
             className={`form-container ${showEditor ? '' : 'hidden'}`}
-            align="center"
+            onValuesChange={handleFormChange}
+            initialValues={props}
+            labelAlign="right"
+            size="middle"
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 20 }}
           >
-            <Form onValuesChange={handleFormChange} initialValues={props}>
-              <Form.Item label="标题" name="title">
-                <Input placeholder="WiFi title"></Input>
-              </Form.Item>
-              <Form.Item label="描述" name="desc">
-                <Input placeholder="WiFi description"></Input>
-              </Form.Item>
-              <Form.Item
-                label="WiFi 名称"
-                name="wifiName"
-                rules={[{ required: true, message: '请输入 WiFi 名称！' }]}
+            <h1>WiFi Card</h1>
+            <Form.Item label="标题" name="title">
+              <Input placeholder="WiFi title"></Input>
+            </Form.Item>
+            <Form.Item label="描述" name="desc">
+              <Input.TextArea
+                placeholder="WiFi description"
+                autoSize={{ minRows: 3, maxRows: 5 }}
+              />
+            </Form.Item>
+            <Form.Item
+              label="WiFi 名称"
+              name="wifiName"
+              rules={[{ required: true, message: '请输入 WiFi 名称！' }]}
+            >
+              <Input placeholder="WiFi name"></Input>
+            </Form.Item>
+            <Form.Item
+              label="WiFi 密码"
+              name="wifiPasswd"
+              rules={[{ required: true, message: '请输入 WiFi 密码！' }]}
+            >
+              <Input placeholder="WiFi password"></Input>
+            </Form.Item>
+            <Form.Item label="显示WiFi信息">
+              <Switch
+                checked={showWifiInfo}
+                onChange={(checked: boolean) => setShowWifiInfoStatus(checked)}
+              ></Switch>
+            </Form.Item>
+            <Form.Item label=" " colon={false}>
+              <Button
+                type="primary"
+                className={`${showEditor ? '' : 'hidden'}`}
+                onClick={handlePrintBtnClick}
+                disabled={!validWifiInfo()}
               >
-                <Input placeholder="WiFi name"></Input>
-              </Form.Item>
-              <Form.Item
-                label="WiFi 密码"
-                name="wifiPasswd"
-                rules={[{ required: true, message: '请输入 WiFi 密码！' }]}
-              >
-                <Input placeholder="WiFi password"></Input>
-              </Form.Item>
-            </Form>
-          </Space>
-          <br />
-          <Card title={props.title}>
+                打印
+              </Button>
+            </Form.Item>
+          </Form>
+          <Card className="wifi-card-container" title={props.title}>
             {props.qrcode ? (
-              <Image src={props.qrcode} />
+              <Image src={props.qrcode} preview={false} />
             ) : (
               <p>请输入正确的 WiFi 名称和密码！</p>
             )}
+            {showWifiInfo ? (
+              <>
+                <p>Wifi 名称：{props.wifiName}</p>
+                <p>Wifi 密码：{props.wifiPasswd}</p>
+                <Divider></Divider>
+              </>
+            ) : null}
             <p>{props.desc}</p>
           </Card>
           <br />
-          <Button
-            type="primary"
-            className={`${showEditor ? '' : 'hidden'}`}
-            onClick={handlePrintBtnClick}
-          >
-            打印
-          </Button>
         </Content>
       </Layout>
     </>
